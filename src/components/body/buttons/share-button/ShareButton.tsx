aa0e0a39047e9e18d1bec3d27783264a5b19bef1
Jeremy <tan_kai_qun97@yahoo.com.sg>
2020-05-21 12:19:50 +0800

    Add copy link to clipboard and support for native sharing

--- src/components/body/buttons/share-button/ShareButton.tsx
+++ src/components/body/buttons/share-button/ShareButton.tsx
@@ -1,7 +1,16 @@
 import React, { useState } from "react";
-import { Icon, Modal, TransitionablePortal, Image } from "semantic-ui-react";
+import {
+  Icon,
+  Modal,
+  TransitionablePortal,
+  Image,
+  Button,
+  Label,
+  Transition,
+} from "semantic-ui-react";
 import { Gif } from "../../gif-item/GifItem";
 import SocialMediaButton from "./social-media-buttons/SocialMediaButtons";
+import CopyToClipBoard from "react-copy-to-clipboard";
 import "../RevealingLabel.scss";
 
 type Props = {
@@ -11,7 +20,14 @@ type Props = {
 
 function ShareButton(props: Props) {
   const [sharing, setSharing] = useState(false);
-  const { gif } = props;
+  const [copied, setCopied] = useState(false);
+  const { title, gifUrl } = props.gif;
+
+  const nativeSharingIsSupported = () => !!(navigator as any).share;
+
+  const share = () => {
+    (navigator as any).share({ url: gifUrl });
+  };
 
   return (
     <>
@@ -34,16 +50,40 @@ function ShareButton(props: Props) {
           onClose={() => setSharing(false)}
           size="small"
         >
-          <Modal.Header>{gif.title}</Modal.Header>
+          <Modal.Header>{title}</Modal.Header>
           <Modal.Content image>
-            <Image src={gif.gifUrl} size="medium" />
+            <Image src={gifUrl} size="medium" />
             <Modal.Description>
-              <a href={gif.gifUrl} target="_blank" rel="noopener noreferrer">
-                <strong>Sharing link</strong>
-              </a>
-              <br />
-              <br />
-              <SocialMediaButton gifUrl={gif.gifUrl} />
+              <p style={{ display: "flex" }}>
+                <CopyToClipBoard
+                  text={gifUrl}
+                  onCopy={() => {
+                    setCopied(true);
+                    setTimeout(() => setCopied(false), 1000);
+                  }}
+                >
+                  <Label as="a">
+                    <strong>Sharing link</strong>
+                  </Label>
+                </CopyToClipBoard>
+                <Transition visible={copied} unmountOnHide duration="250">
+                  <span style={{ paddingInlineStart: "1rem" }}>
+                    Link copied
+                  </span>
+                </Transition>
+              </p>
+              <p>
+                {nativeSharingIsSupported() ? (
+                  <Button
+                    primary
+                    compact
+                    content="Share to other apps"
+                    onClick={share}
+                  />
+                ) : (
+                  <SocialMediaButton gifUrl={gifUrl} />
+                )}
+              </p>
             </Modal.Description>
           </Modal.Content>
         </Modal>
